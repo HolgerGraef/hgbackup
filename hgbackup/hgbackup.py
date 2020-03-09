c4fe326f758f7ce5de593d60879beed4ad3c7333
Holger Graef <holger.graef@gmail.com>
2020-03-09 17:21:39 +0100

    added window icon

--- hgbackup/hgbackup.py
+++ hgbackup/hgbackup.py
@@ -3,11 +3,13 @@ from .hgbcore import HGBCore
 from .hgbcli import HGBCLI
 from .hgbgui import HGBGUI
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 
 def main():
     hgbcore = HGBCore()
     if len(sys.argv) == 1 or (len(sys.argv) == 2 and sys.argv[1] == 'hidden'):
         app = QApplication(sys.argv)
+        app.setWindowIcon(QIcon.fromTheme('task-due'))
 
         hgbgui = HGBGUI(hgbcore)
         if len(sys.argv) == 1:
